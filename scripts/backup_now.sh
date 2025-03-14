3f1830cab646272bec5f8dde83fffac21177ff61
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2025-03-14 15:29:30 +0100

    Update backup_now.sh

--- scripts/backup_now.sh
+++ scripts/backup_now.sh
@@ -9,17 +9,21 @@ ftp_user=$(ynh_app_setting_get --app=$app --key=ftp_user)
 ftp_password=$(ynh_app_setting_get --app=$app --key=ftp_password)
 ftp_path=$(ynh_app_setting_get --app=$app --key=ftp_path)
 
-# Backup erstellen und auf FTP-Server hochladen
-ynh_print_info "Starting backup and FTP upload..."
+# Backup erstellen
+ynh_print_info "Erstelle YunoHost backup..."
+backup_file="yunohost_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+yunohost backup create --output-directory /home/yunohost.backup/archives/ --name $backup_file
 
-# Hier Ihren Backup- und Upload-Code einfügen
-# Beispiel:
-# yunohost backup create
-# ftp -inv $ftp_server << EOF
-# user $ftp_user $ftp_password
-# cd $ftp_path
-# put /path/to/backup/file
-# quit
-# EOF
+# Backup auf FTP-Server hochladen
+ynh_print_info "Lade das Backup auf den FTP Server..."
+ftp -inv $ftp_server << EOF
+user $ftp_user $ftp_password
+cd $ftp_path
+put /home/yunohost.backup/archives/$backup_file
+quit
+EOF
 
-ynh_print_info "Backup and FTP upload completed."
+# Aufräumen
+rm /home/yunohost.backup/archives/$backup_file
+
+ynh_print_info "Backup and FTP upload abgeschlossen."
